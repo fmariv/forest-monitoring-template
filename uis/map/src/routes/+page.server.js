d8d8a3ae9b934533eaac441afba6094064e2a8a8
Fran Martin <fran@earthpulse.ai>
2024-09-04 14:16:00 +0200

    update

--- uis/map/src/routes/+page.server.js
+++ uis/map/src/routes/+page.server.js
@@ -1,8 +1,8 @@
 import { env } from '$env/dynamic/private';
 
 export async function load({fetch}) {
-	const xyz_url = 'http://' +  env.XYZ_URL;
-	const analytics_url = 'http://' +  env.ANALYTICS_URL;
+	const xyz_url = 'https://' +  env.XYZ_URL;
+	const analytics_url = 'https://' +  env.ANALYTICS_URL;
 	let res = [
 		await fetch(`${xyz_url}/`),
 		await fetch(`${analytics_url}/AOI_Vegetation_Growth`),
