1cc42e95a281bc7d324926c36abe2ecf48caa838
Fran Martin <fmartinrivas2@gmail.com>
2025-07-23 18:11:19 +0200

    Update requirements

--- uis/map/src/routes/+page.server.js
+++ uis/map/src/routes/+page.server.js
@@ -1,7 +1,10 @@
 import { env } from '$env/dynamic/private';
 
 export async function load({fetch}) {
-	const api_url = 'https://' +  env.API_URL;
+	const ENV = import.meta.env.VITE_ENV;
+    let origin = ENV === 'PRO' ? 'https://' : 'http://';
+    const api_url = `${origin}${env.API_URL}`;
+
 	let res = [
 		await fetch(`${api_url}/images`),
 		await fetch(`${api_url}/analytics/AOI_Vegetation_Growth`),
